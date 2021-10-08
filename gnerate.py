c63f511df0247447a0ef613a9755aa1df6fa6ad3
Marcin Walesa <mwalesa@man.poznan.pl>
2021-10-08 12:12:37 +0200

    - hexagon on border fixing continue

--- gnerate.py
+++ gnerate.py
@@ -15,37 +15,35 @@ def calc_x(line_params, y):
     return x
 
 
-def fit_points_to_the_limit(points, points_in_range, limit):
+def fit_points_to_the_limit(points_w_md, limit):
 
-    point_count = len(points)
-    partialy_corrected_points = []
-
-    for i in range(point_count):
-        partialy_corrected_points.append(None)
-
-
+    point_count = len(points_w_md)
 
     for i in range(point_count):
 
-        pir = points_in_range[i]
-        por = points[i]
+        pir = points_w_md[i]["point"]
+        pirvc = points_w_md[i]["vc"]
+
         prev_pir_idx = i-1 if i != 0 else point_count-1
         next_pir_idx = i+1 if i != point_count-1 else 0
-        prev_pir = points_in_range[prev_pir_idx]
-        next_pir = points_in_range[next_pir_idx]
+
+        prev_pir = points_w_md[prev_pir_idx]["point"]
+        prev_pirvc = points_w_md[prev_pir_idx]["vc"]
+        next_pir = points_w_md[next_pir_idx]["point"]
+        next_pirvc = points_w_md[next_pir_idx]["vc"]
 
         pcalc = None
-        if pir == None:
-            if prev_pir == None and next_pir == None:
+        if pirvc != None:
+            if prev_pirvc != None and next_pirvc != None:
                 continue
 
-            pcalc = {"x": por["x"], "y": por["y"]}
+            pcalc = {"x": pir["x"], "y": pir["y"]}
             line_point = {"x": 0, "y": 0}
-            if prev_pir != None:
+            if prev_pirvc == None:
                 line_point = prev_pir
 
-            if next_pir != None:
-                line_point = points[next_pir_idx]
+            if next_pirvc == None:
+                line_point = next_pir
 
             lp = calc_line_params(pcalc, line_point)
             x_target = (limit["x.min"] if  pcalc["x"] < limit["x.min"] else None)
@@ -71,44 +69,186 @@ def fit_points_to_the_limit(points, points_in_range, limit):
         else:
             pcalc = pir
 
-        partialy_corrected_points[i] = pcalc
+        points_w_md[i]["point"] = pir
 
-        
-    return partialy_corrected_points
+    return points_w_md
 
-def fit_points_to_the_limit_2(points, points_in_range, limit):
-
-    point_count = len(points)
+def generate_hexagon_points_flow_description(points_in_range):
+    point_count = len(points_in_range)
     meta_description = []
 
-    neead_to_fix = False
-    last_state = None
     for i in range(point_count):
         point_description = {}
         next_point_idx = i+1 if i != point_count-1 else 0
 
         pir = points_in_range[i]
+        next_pir = points_in_range[next_point_idx]
 
-        state = None
-        if pir == None:
-            neead_to_fix = True
-            state = "out"
-        else:
-            state = "in"
+        pir_in = pir != None
+        npir_in = next_pir != None
+        cond = not pir_in or not npir_in
 
-        if state != last_state:
-            last_state = state
-            point_description["idx"] = i
-            point_description["state"] = state
+
+        if pir_in and not npir_in:
+            point_description["state"] = "in->out"
+
+        if not pir_in and not npir_in:
+            point_description["state"] = "out"
+
+        if not pir_in and npir_in:
+            point_description["state"] = "out->in"
+
+        if cond:
+            point_description["from"] = i
+            point_description["to"] = next_point_idx
             meta_description.append(point_description)
 
-        # tu dodać analizę lini w stosunku do poprzedniego punktu czy tam następnego
+    desc_count = len(meta_description)
+    sorted_desc_count = 0
 
-    if neead_to_fix:
-        print(meta_description)
+    found = False
+    desc_pinter = 0
+    sorted_meta_description = []
+    while sorted_desc_count != desc_count:
+        this_iter_desc = meta_description[desc_pinter]
 
+        if this_iter_desc["state"] == "in->out":
+            found = True
+
+        if found:
+            sorted_meta_description.append(this_iter_desc)
+            sorted_desc_count = sorted_desc_count + 1
+
+        desc_pinter = 0 if desc_pinter == desc_count - 1 else desc_pinter + 1
+
+    return sorted_meta_description
+
+def point_in_limit(point, limit):
+    xmaxconda = point["x"] <= limit["x.max"]
+    xminconda = point["x"] >= limit["x.min"]
+    ymaxconda = point["y"] <= limit["y.max"]
+    yminconda = point["y"] >= limit["y.min"]
+
+def extend_hexagon_points_flow_description(meta_desc, points, limit):
+    for desc in meta_desc:
+        if desc["status"] == "out":
+            continue
+        else:
+            pa = points[desc["from"]]
+            pb = points[desc["to"]]
+            line_params = calc_line_params(pa, pb)
+
+            dirs = []
+            xmaxconda = pa["x"] <= limit["x.max"]
+            xminconda = pa["x"] >= limit["x.min"]
+            ymaxconda = pa["y"] <= limit["y.max"]
+            yminconda = pa["y"] >= limit["y.min"]
+
+            xmaxcondb = pb["x"] <= limit["x.max"]
+            xmincondb = pb["x"] >= limit["x.min"]
+            ymaxcondb = pb["y"] <= limit["y.max"]
+            ymincondb = pb["y"] >= limit["y.min"]
+
+            if xmaxconda != xmaxcondb:
+                dirs.append("x.max")
+
+            if xminconda != xmincondb:
+                dirs.append("x.min")
+
+            if ymaxconda != ymaxcondb:
+                dirs.append("y.max")
+
+            if yminconda != ymincondb:
+                dirs.append("y.min")
+
+            cross_points = []
+            for dir in dirs:
+                cross_info = {"dir": dir}
+                if dir == "y.min" or dir == "y.max":
+                    cross_info["point"] = {"x": calc_x(line_params, limit[dir]), "y": limit[dir]}
+                if dir == "x.min" or dir == "x.max":
+                    cross_info["point"] = {"y": calc_y(line_params, limit[dir]), "x": limit[dir]}
+                cross_points.append(cross_info)
+
+            
+
+
+
+def check_limit_crosses(p1, p2, limit):
+
+    line_params = calc_line_params(p1, p2)
+
+    y = calc_y(line_params, limit["x.min"])
+    pa = {"x": limit["x.min"], "y": y}
+
+    x = calc_x(line_params, limit["y.min"])
+    pb = {"x": x, "y": limit["y.min"]}
+    
+    y = calc_y(line_params, limit["x.max"])
+    pc = {"x": limit["x.min"], "y": y}
+
+    x = calc_x(line_params, limit["y.max"])
+    pd = {"x": x, "y": limit["y.min"]}
+
+    crosses = [pa, pb, pc, pd]
+    in_range_crosses = []
+    for cross in crosses:
+        xmaxcond = cross["x"] <= limit["x.max"]
+        xmincond = cross["x"] >= limit["x.min"]
+        ymaxcond = cross["y"] <= limit["y.max"]
+        ymincond = cross["y"] >= limit["y.min"]
+
+        if xmaxcond and xmincond and ymaxcond and ymincond:
+            in_range_crosses.append(cross)
+
+
+    irc_count = len(in_range_crosses)
+    if irc_count >= 2:
+        if irc_count == 2:
+            # check which is cloaser to p1
+            print(f" były dwa crossy {in_range_crosses}")
+            xd1 = p1["x"]-in_range_crosses[0]["x"]
+            xd2 = p1["x"]-in_range_crosses[1]["x"]
+            yd1 = p1["y"]-in_range_crosses[0]["y"]
+            yd2 = p1["y"]-in_range_crosses[1]["y"]
+
+            sqp1dist = xd1*xd1 + yd1*yd1
+            sqp2dist = xd2*xd2 + yd2*yd2
+
+            if sqp2dist < sqp1dist:
+                cross1 = in_range_crosses[0]
+                cross2 = in_range_crosses[1]
+
+                in_range_crosses[0] = cross2
+                in_range_crosses[1] = cross1
+        else:
+            # should never happened
+            print(f" były więcej niż dwa crossy")
+
+    return in_range_crosses
+     
+
+def fit_points_to_the_limit_2(points, points_in_range, limit):
+    meta_desc = generate_hexagon_points_flow_description(points_in_range)
+    meta_desc = extend_hexagon_points_flow_description(meta_desc, points)
+
+    if len(meta_desc) == 0:
+        return points_in_range
+
+    print(meta_desc)
+    crosses = []
+    for desc in meta_desc:
+        p1 = points[desc["from"]]
+        p2 = points[desc["to"]]
+
+        limit_crosses = check_limit_crosses(p1, p2, limit)
+        crosses.append(limit_crosses)
+
+
+    print(crosses)   
 
     return points_in_range
+    
         
 
 def generateHexagonPoints(size, position, phase, limit):
@@ -128,31 +268,44 @@ def generateHexagonPoints(size, position, phase, limit):
 
         points.append({"x": x, "y": y})
 
-    points_in_range = []
+    points_w_md = []
     out_of_range = True
     for point in points:
         xmaxcond = point["x"] > limit["x.max"]
         xmincond = point["x"] < limit["x.min"]
         ymaxcond = point["y"] > limit["y.max"]
         ymincond = point["y"] < limit["y.min"]
+        md_point = {"point": point}
         if xmaxcond or xmincond or ymaxcond or ymincond:
-            points_in_range.append(None)
+            vialoate_cond = []
+            if xmaxcond:
+                vialoate_cond.append("x.max")
+            if xmincond:
+                vialoate_cond.append("x.min")
+            if ymaxcond:
+                vialoate_cond.append("y.max")
+            if ymincond:
+                vialoate_cond.append("y.min")
+
+            # tu by można dodać
+            md_point["vc"] = vialoate_cond
         else:
-            points_in_range.append(point)
+            md_point["vc"] = None
             out_of_range = False
+
+        points_w_md.append(md_point)
 
     if out_of_range:
         return None
-    fit_points_to_the_limit_2(points, points_in_range, limit)
-    limited_points = fit_points_to_the_limit(points, points_in_range, limit)
+    # fit_points_to_the_limit_2(points_w_md, limit)
+    limited_points = fit_points_to_the_limit(points_w_md, limit)
     return limited_points
 
 
-def generatePathDescription(points):
+def generatePathDescription(points_w_md):
     d = []
-    for point in points:
-        if point is None:
-            continue
+    for point_w_md in points_w_md:
+        point = point_w_md["point"]
 
         if(len(d) == 0):
             d.append(f'M {point["x"]} {point["y"]} ')
